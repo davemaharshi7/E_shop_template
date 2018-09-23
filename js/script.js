3f60f784195ddfdb293af7d9d850f507a2c05efd
davemaharshi7 <39943075+davemaharshi7@users.noreply.github.com>
2018-09-23 21:55:35 +0530

    2nd Commit
    2nd Commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-        document.getElementById("myBtn").style.display = "block";
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("toTop").style.display = "block";
     } else {
-        document.getElementById("myBtn").style.display = "none";
+        document.getElementById("toTop").style.display = "none";
     }
 }
 
@@ -14,3 +14,30 @@ function topFunction() {
     document.documentElement.scrollTop = 0;
 }
 
+// $(document).ready(function() {
+//     // scroll down
+//     $("html, body").animate({
+//         scrollTop: $(document).height()
+//     }, 1000);                  
+
+//     //scroll back up
+//     $("html, body").animate({
+//         scrollTop: 101
+//     }, 1000);                  
+// });
+
+// $(window).scroll(function() {
+//     if ($(this).scrollTop()) {
+//         $('#toTop').fadeIn();
+//     } else {
+//         $('#toTop').fadeOut();
+//     }
+// });
+
+// $("#toTop").click(function () {
+// //1 second of animation time
+// //html works for FFX but not Chrome
+// //body works for Chrome but not FFX
+// //This strange selector seems to work universally
+// $("html, body").animate({scrollTop: 0}, 1000);
+// });
